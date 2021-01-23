b5670cb26639b5a471c6cf7a605e6dcd9b88837d
Ignat <ignat14@gmail.com>
2021-01-23 07:42:33 +0100

    set default secret key

--- application/settings.py
+++ application/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'lsa79824yhlkjf98273ihfsdlknc89324')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
