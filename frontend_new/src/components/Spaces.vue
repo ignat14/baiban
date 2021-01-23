05d26c53be4ab39824917951fed88d17fab4d350
Ignat <ignat14@gmail.com>
2021-01-23 07:21:42 +0100

    add the frontend folder

---
+++ frontend_new/src/components/Spaces.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Spaces</h1>
+    <ul v-for="space in spaces" :key="space.id">
+      <li>
+        <span>{{ space.name }}</span>
+        <img :src="space.avatar" width="150" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import axios from 'axios';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class Spaces extends Vue {
+  msg!: string
+  spaces: object[] = []
+
+  async created() {
+    const response = await axios.get('api/v1/space/space/')
+    this.spaces = response.data
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    margin: 10px;
+  }
+}
+</style>
---
+++ frontend_new/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Spaces />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Spaces from '@/components/Spaces.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    Spaces,
+  },
+})
+export default class Home extends Vue {}
+</script>
